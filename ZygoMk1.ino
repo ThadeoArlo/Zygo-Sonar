547fbbe0262f56c781e7fe02cf9ab78e7943b648
Thadeo Arlo <thadeoarlo@gmail.com>
2018-04-27 20:58:35 +0700

    updated code

--- ZygoMk1.ino
+++ ZygoMk1.ino
@@ -1,30 +1,28 @@
-/*Modified version of obstacle avoider,
-code adapted to detect proximity and 
-sends signal if rang is less than ~45cm*/
-#define trigPin 12  // define the pins of your sensor
+#define trigPin 12
 #define echoPin 13
-int ledPin = 11;  // LED connected to digital pin 13
+int ledPin = 11;
 void setup() {
-  pinMode(ledPin, OUTPUT);   // sets the digital pin as output
-  Serial.begin(9600);        
-  pinMode(trigPin, OUTPUT);  
-  pinMode(echoPin, INPUT);   
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
+
 void loop() {
-  long duration, distance;  // start the scan
+  long duration, distance;
   digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);  // delays are required for a succesful sens or operation.
+  delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);  // this delay is required as well!
+  delayMicroseconds(10);
   duration = pulseIn(echoPin, HIGH);
-  distance = (duration / 2) / 29.1;  // convert the distance to centimeters
-  if (distance < 100) { /*if there's an obstacle 25 centimers, ahead, do the following: */
+  distance = (duration / 2) / 29.1;
+  if (distance < 30) {
     Serial.println("Close Obstacle detected!");
     Serial.print(distance);
-    Serial.print(" CM!");  // print out the distance in centimeters.
-    digitalWrite(ledPin, HIGH);  // sets the LED on
+    Serial.print(" CM!");
+    digitalWrite(ledPin, HIGH);
   } else {
     Serial.println("No obstacle detected.");
-    digitalWrite(ledPin, LOW);  // sets the LED off
+    digitalWrite(ledPin, LOW);
   }
 }
